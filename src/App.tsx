cebdd3cc68205e46bf8198c32fd7cc582263d3c1
Paul Miyamoto <mfsaru@me.com>
2025-11-01 21:12:00 +0900

    Add About page with project description and navbar link

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Explore from "./routes/Explore";
 import Gallery from "./routes/Gallery";
 import SpotDetail from "./routes/SpotDetail";
+import About from "./routes/About";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Explore />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/spots/:id" element={<SpotDetail />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
